495eecd6d3af949008c3c881822baceaa0a8e861
Greg Federico <greg.federico@tivix.com>
2018-08-15 12:27:04 -0400

    PostList, Post
    Created Post component. Each post is rendered in the PostList view, displaying title and content.

---
+++ src/components/Post.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="post">
+      <h2 class="post-header">{{ postData.title }}</h2>
+      <p>{{ postData.content }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Post",
+    props: {
+        postData: null
+    },
+    mounted() {
+        console.log("this.postData", this.postData);
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+// h3 {
+//     margin: 40px 0 0;
+// }
+// ul {
+//     list-style-type: none;
+//     padding: 0;
+// }
+// li {
+//     display: inline-block;
+//     margin: 0 10px;
+// }
+a {
+    color: #42b983;
+}
+</style>
--- src/views/PostList.vue
+++ src/views/PostList.vue
@@ -2,14 +2,25 @@
   <div class="post-list">
       <h1>All Posts</h1>
       <!-- Will loop thru Post components here -->
+      <ul>
+          <li v-for="post in posts" v-bind:key="post.id">
+              <Post v-bind:postData="post" />  
+          </li>
+      </ul>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import Post from "@/components/Post.vue";
+
 const baseUrl = "http://localhost:8004/api/";
 
 export default {
+    name: "home",
+    components: {
+        Post
+    },
     data: function() {
         return {
             posts: []
@@ -24,7 +35,7 @@ export default {
                 .get(baseUrl)
                 .then(response => {
                     this.posts = response.data;
-                    console.log(this.posts);
+                    // console.log(this.posts);
                 })
                 .catch(error => {
                     console.log(error);
@@ -33,3 +44,23 @@ export default {
     }
 };
 </script>
+
+
+<style lang="scss">
+// Removed scoped attr because it doesn't seem to work in views the same way as with components..
+
+// h3 {
+//     margin: 40px 0 0;
+// }
+ul {
+    list-style-type: none;
+    // padding: 0;
+}
+// li {
+//     display: inline-block;
+//     margin: 0 10px;
+// }
+a {
+    color: #42b983;
+}
+</style>
