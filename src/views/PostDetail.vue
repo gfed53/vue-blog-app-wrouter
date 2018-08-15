885517eb07d84f99f43e949f9fff39ef9d24a446
Greg Federico <greg.federico@tivix.com>
2018-08-15 14:33:32 -0400

    Post dates
    Installed moment package.
    
    Created computed properties for post’s date created and updated. Rendering formatted date created in the view along with date updated if different from created.
    
    Some style updates.

--- src/views/PostDetail.vue
+++ src/views/PostDetail.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="post-detail">
         <h2 class="post-header">{{ post.title }}</h2>
+        <div class="post-meta">
+            <p> <strong>Created:</strong> {{ dateCreated }}</p>
+            <p v-if="dateCreated !== dateUpdated"><strong>Last Updated:</strong> {{ dateUpdated }}</p>
+        </div>
         <p>{{ post.content }}</p>
 
         <h3>Comments:</h3>
@@ -14,6 +18,7 @@
 
 <script>
 import axios from "axios";
+import moment from "moment";
 
 const baseUrl = "http://localhost:8004/api/";
 
@@ -55,10 +60,32 @@ export default {
                     console.log(error);
                 });
         }
+    },
+    computed: {
+        dateCreated: function() {
+            return moment(this.post.created_at).format("MM/DD/YYYY, h:mm:ss a");
+        },
+        dateUpdated: function() {
+            return moment(this.post.updated_at).format("MM/DD/YYYY, h:mm:ss a");
+        }
     }
 };
 </script>
 
 
 <style lang="scss">
+.post-detail {
+    max-width: 768px;
+    margin: auto;
+    .post-meta {
+        padding: 0.5em;
+        border-top: 1px solid lightgray;
+        border-bottom: 1px solid lightgray;
+        p {
+            color: #42b983;
+            font-size: 0.9em;
+            margin: 0;
+        }
+    }
+}
 </style>
