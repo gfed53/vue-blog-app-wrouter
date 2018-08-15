4f2de2206700a29bc92707a3996467f6dc09fa1e
Greg Federico <greg.federico@tivix.com>
2018-08-15 12:05:35 -0400

    Axios, PostList
    Installed axios package.
    
    Created PostList view that will display all posts from back end. Logging posts to console for now.

---
+++ src/views/PostList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="post-list">
+      <h1>All Posts</h1>
+      <!-- Will loop thru Post components here -->
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+const baseUrl = "http://localhost:8004/api/";
+
+export default {
+    data: function() {
+        return {
+            posts: []
+        };
+    },
+    mounted() {
+        this.getPosts();
+    },
+    methods: {
+        getPosts() {
+            axios
+                .get(baseUrl)
+                .then(response => {
+                    this.posts = response.data;
+                    console.log(this.posts);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    }
+};
+</script>
